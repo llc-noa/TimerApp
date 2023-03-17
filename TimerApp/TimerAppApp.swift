80b6921094a91e22ee9f2054e9c0cc06196536f0
rsugaya <rsugaya@llc-noa.net>
2023-03-17 18:32:40 +0900

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by 菅谷亮太 on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
